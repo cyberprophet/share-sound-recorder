d18ceafe8f9bfcdaa2a1a431811e5ae0bc9adc70
SANGWOO PARK <prophet0915@gmail.com>
2025-01-03 11:25:05 +0900

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 1.0.0
+
+- transmit voice recordings in real time.
---
+++ lib/share_sound_recorder.dart
@@ -0,0 +1,7 @@
+library share_sound_recorder;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
